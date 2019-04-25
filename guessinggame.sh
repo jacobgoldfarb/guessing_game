05597cf60648d5312c52b47f4753d55e2c27716d
jacobgoldfarb <jacobgoldfarb@Bernards-iMac.local>
2019-04-24 22:53:17 -0400

    Added function to program.

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,23 +4,24 @@
 guess=-1
 files_count=$(($(ls -1 | wc -l)+0))
 
+function compare {
+   if [[ $1 -gt $2 ]]
+        then
+                echo "Too high."
+        elif [[ $1 -lt $2 ]]
+        then
+                echo "Too low."
+        elif [[ $1 -eq $2 ]]
+        then
+                echo "You got it!"
+        else
+                echo "Program error: invalid input"
+        fi	
+}
+
 while [[ $guess -ne $files_count ]]
 do
 	echo "How man files are in this directory?"
 	read guess
-	echo "Guess $guess files $files_count"
-	
-	if [[ $guess -gt $files_count ]]
-	then
-		echo "Too high."
-	elif [[ $guess -lt $files_count ]]
-	then
-		echo "Too low."
-	elif [[ $guess -eq $files_count ]]
-	then
-		echo "You got it!"
-	else
-		echo "Program error: invalid input"
-	fi
+	compare $guess $files_count
 done
-
